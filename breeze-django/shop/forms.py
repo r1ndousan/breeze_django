0ba7165d5a11c3b2786ca651bccf8fb8a9f99b67
rindousan <rindousan@yandex.ru>
2025-12-13 23:38:30 +0300

    news3
    news form

--- breeze-django/shop/forms.py
+++ breeze-django/shop/forms.py
@@ -7,4 +7,20 @@ class NewsForm(forms.ModelForm):
         fields = ['title', 'slug', 'content', 'image', 'published_at']
         widgets = {
             'published_at': forms.DateInput(attrs={'type': 'date'}),
+            'title': forms.TextInput(attrs={
+                'class': 'input news-title-input',
+                'style': 'width: 100%; height: 48px;',
+                'placeholder': 'Введите заголовок новости'
+            }),
+            'slug': forms.TextInput(attrs={
+                'class': 'input',
+                'style': 'width: 100%; height: 48px;',
+                'placeholder': 'Slug'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'input news-form__content',
+                'placeholder': 'Введите текст новости',
+                'rows': 10,
+                'style': 'width: 100%; resize: vertical;',
+            }),
         }
